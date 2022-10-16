dbd70e4b8d3182f3797799a1db12bd7faeeb07da
onefootfarina <clichecontent@gmail.com>
2022-10-16 15:32:51 -0400

    had it but harder test made it fail

---
+++ 66-PlusOne/plusone.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} digits
+ * @return {number[]}
+ */
+
+function plusOne(digits) {
+  let num = "";
+  let parseNumPlusOne;
+  let finalArray = [];
+
+  digits.forEach((digit) => {
+    num += digit;
+  });
+
+  parseNumPlusOne = num;
+
+  console.log(parseNumPlusOne);
+  for (let i = 0; i < parseNumPlusOne.length; i++) {
+    // finalArray.push(parseNumPlusOne[i]);
+  }
+
+  console.log(finalArray);
+  return finalArray;
+}
+
+plusOne([1, 2, 3]); // [1,2,4]
+plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]);
