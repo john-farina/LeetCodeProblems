8b11a8b87cadfdb48c872e11771c35caa47c580c
onefootfarina <clichecontent@gmail.com>
2022-10-20 17:27:25 -0400

    not sure what changed

--- 14-LongestPrefix/prefix.js
+++ 14-LongestPrefix/prefix.js
@@ -6,10 +6,16 @@
 //go through each array and see if they all equal f
 //then do it again and see if it is the same
 function longestCommonPrefix(strings) {
-  let prefix = [];
+  let prefix = "f";
+  let count = 0;
   for (let i = 0; i < strings.length; i++) {
-    prefix.push(strings[i][0]);
+    if (strings[i][count] === prefix) {
+      console.log("hello", i);
+      // console.log(prefix);
+    }
+    count++;
   }
+  return;
   console.log(prefix);
 }
 
