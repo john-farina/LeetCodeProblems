fa400c335e048d18584ce2c86f3bdb2f58717d77
onefootfarina <clichecontent@gmail.com>
2022-10-20 23:26:36 -0400

    got the counting of bits to work lol

--- 191-NumberOf1Bits/bits.js
+++ 191-NumberOf1Bits/bits.js
@@ -2,4 +2,18 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
-function hammingWeight(n) {}
+function hammingWeight(n) {
+  let string = BigInt(n).toString(2);
+  let oneCount = 0;
+
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === "1") {
+      oneCount++;
+    }
+  }
+
+  console.log(oneCount);
+  return oneCount;
+}
+
+hammingWeight(00000000000000000000000000001011);
