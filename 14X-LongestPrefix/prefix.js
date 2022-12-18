a5256b8be0f7ca554db4c7bebabdd4b8793e723e
john-farina <clichecontent@gmail.com>
2022-12-18 12:03:00 -0500

    gonna do some leetcode

--- 14X-LongestPrefix/prefix.js
+++ 14X-LongestPrefix/prefix.js
@@ -3,19 +3,16 @@
  * @return {string}
  */
 // why is this hard lol
-//go through each array and see if they all equal f
-//then do it again and see if it is the same
+// go through each array and see if they all equal f
+// then do it again and see if it is the same
+
 function longestCommonPrefix(strings) {
-  let prefix = undefined;
+  let prefix = "";
 
   for (string of strings) {
     console.log(string);
-    for (letter in string) {
-      if (prefix === undefined) {
-        console.log(string[letter]);
-        prefix += string[letter];
-        break;
-      }
+    for (letter of string) {
+      console.log(letter);
     }
   }
 }
