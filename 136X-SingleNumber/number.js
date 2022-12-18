263afcec5788ca6b7382f381bc2bf1b0cdefbacc
john-farina <clichecontent@gmail.com>
2022-12-18 13:13:27 -0500

    need to learn more algorithm methods

--- 136X-SingleNumber/number.js
+++ 136X-SingleNumber/number.js
@@ -4,14 +4,7 @@
  */
 
 function singleNumber(nums) {
-  for (let i = 0; i < nums.length; i++) {
-    console.log("first loop", nums[i]);
-    for (let l = 0; l < nums.length; l++) {
-      console.log("second loop", nums[l]);
-      if (nums[l] === nums[i]) {
-      }
-    }
-  }
+  let singleNum = [];
 }
 
-singleNumber([1, 2, 3]); //should return 1
+singleNumber([1, 2, 2]); //should return 1
