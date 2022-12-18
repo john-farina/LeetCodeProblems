7488a5bf8322ef517a41d033de36be300ce203a1
john-farina <clichecontent@gmail.com>
2022-12-18 12:07:37 -0500

    yeah i forgot this ones hard

--- 14X-LongestPrefix/prefix.js
+++ 14X-LongestPrefix/prefix.js
@@ -6,14 +6,13 @@
 // go through each array and see if they all equal f
 // then do it again and see if it is the same
 
-function longestCommonPrefix(strings) {
-  let prefix = "";
+const { string } = require("prop-types");
 
-  for (string of strings) {
-    console.log(string);
-    for (letter of string) {
-      console.log(letter);
-    }
+function longestCommonPrefix(strings) {
+  let prefix = undefined;
+
+  for (let i = 0; i < strings.length; i++) {
+    let string = strings[i];
   }
 }
 
