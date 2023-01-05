b5b94ddd973275e9cc78f8bbd936eef1d77f5e3d
john-farina <clichecontent@gmail.com>
2023-01-04 22:59:23 -0500

    added folder couldnt finish

---
+++ 1-TwoSum/twosum.js
@@ -0,0 +1,9 @@
+function twoSum(nums, target) {
+  // go through array choose num (go through array again and see if any other numbers add to the target)
+  // if (not) then go again till i find a match
+  let firstNum;
+
+  for (let i = 0; i < nums.length; i++) {}
+}
+
+twoSum([2, 7, 11, 15], 9);
