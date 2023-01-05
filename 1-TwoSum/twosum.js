b99563d141cd9e62f331ec9f68a9661b2753aad1
john-farina <clichecontent@gmail.com>
2023-01-05 11:22:09 -0500

    made two sums but can optimize it

--- 1-TwoSum/twosum.js
+++ 1-TwoSum/twosum.js
@@ -1,9 +1,21 @@
 function twoSum(nums, target) {
-  // go through array choose num (go through array again and see if any other numbers add to the target)
-  // if (not) then go again till i find a match
-  let firstNum;
+  // get the first val in an array
+  // loop through each other val and do math to see if it equals target
+  // if not keep going
+  let savedVal;
 
-  for (let i = 0; i < nums.length; i++) {}
+  for (let i = 0; i < nums.length; i++) {
+    savedVal = nums[i];
+
+    for (let l = i + 1; l < nums.length; l++) {
+      if (nums[i] + nums[l] === target) {
+        console.log("passed: ", [i, l]);
+        return [i, l];
+      }
+    }
+  }
 }
 
 twoSum([2, 7, 11, 15], 9);
+twoSum([3, 2, 4], 6);
+twoSum([3, 3], 6);
